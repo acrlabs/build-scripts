7af084bd9d0bc9cf1ef8c8d2940dd5314ab45ef0
David Morrison <drmorr@appliedcomputing.io>
2024-01-24 09:51:44 -0800

    use poetry update every time instead of poetry install once

--- k8s.mk
+++ k8s.mk
@@ -3,12 +3,10 @@ K8S_MANIFESTS_DIR ?= $(BUILD_DIR)/manifests
 .PHONY: k8s
 
 k8s:
+	cd k8s && poetry update
 	cd k8s && CDK8S_OUTDIR=$(K8S_MANIFESTS_DIR) BUILD_DIR=$(BUILD_DIR) poetry run ./main.py
 
 run: k8s
 	cp -r k8s/raw $(K8S_MANIFESTS_DIR) || true
 	kubectl apply -f $(K8S_MANIFESTS_DIR)/raw || true
 	kubectl apply -f $(K8S_MANIFESTS_DIR)
-
-setup::
-	cd k8s && poetry install
