6d20cfd40b9dda4b301521b42eac2ad62e2cc9e3
David Morrison <drmorr@appliedcomputing.io>
2024-01-29 12:55:54 -0800

    silence annoying node version error

--- k8s.mk
+++ k8s.mk
@@ -4,7 +4,7 @@ K8S_MANIFESTS_DIR ?= $(BUILD_DIR)/manifests
 
 k8s:
 	cd k8s && poetry update
-	cd k8s && CDK8S_OUTDIR=$(K8S_MANIFESTS_DIR) BUILD_DIR=$(BUILD_DIR) poetry run ./main.py
+	cd k8s && JSII_SILENCE_WARNING_UNTESTED_NODE_VERSION=1 CDK8S_OUTDIR=$(K8S_MANIFESTS_DIR) BUILD_DIR=$(BUILD_DIR) poetry run ./main.py
 
 run: k8s
 	cp -r k8s/raw $(K8S_MANIFESTS_DIR) || true
