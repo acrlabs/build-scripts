0b7763f8d18bf5245df99bf133d9ec805b8d31c2
David Morrison <drmorr@appliedcomputing.io>
2024-03-11 21:55:03 -0700

    poetry install instead of poetry update

--- k8s.mk
+++ k8s.mk
@@ -3,7 +3,7 @@ K8S_MANIFESTS_DIR ?= $(BUILD_DIR)/manifests
 .PHONY: k8s
 
 k8s:
-	cd k8s && poetry update
+	cd k8s && poetry install
 	cd k8s && JSII_SILENCE_WARNING_UNTESTED_NODE_VERSION=1 CDK8S_OUTDIR=$(K8S_MANIFESTS_DIR) BUILD_DIR=$(BUILD_DIR) poetry run ./main.py
 
 run: k8s
