d759f29628c7cb092e8d8fe730dbf94d3b27e3ca
David Morrison <drmorr@appliedcomputing.io>
2024-06-17 11:37:19 -0700

    add fc args to k8s target

--- k8s.mk
+++ k8s.mk
@@ -4,7 +4,7 @@ K8S_MANIFESTS_DIR ?= $(BUILD_DIR)/manifests
 
 k8s:
 	cd k8s && poetry install
-	cd k8s && JSII_SILENCE_WARNING_UNTESTED_NODE_VERSION=1 CDK8S_OUTDIR=$(K8S_MANIFESTS_DIR) BUILD_DIR=$(BUILD_DIR) poetry run ./main.py
+	cd k8s && JSII_SILENCE_WARNING_UNTESTED_NODE_VERSION=1 CDK8S_OUTDIR=$(K8S_MANIFESTS_DIR) BUILD_DIR=$(BUILD_DIR) poetry run ./main.py $(FC_ARGS)
 
 run: k8s
 	cp -r k8s/raw $(K8S_MANIFESTS_DIR) || true
