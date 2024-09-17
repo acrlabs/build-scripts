86c51c2e8425925cf004fcbada10129c0b3ec855
balta-z-r <zunigaruizb@gmail.com>
2024-09-16 17:37:40 -0700

    fix

--- docker_tag.sh
+++ docker_tag.sh
@@ -6,14 +6,14 @@ SHA=$(git rev-parse --short HEAD)
 TAGS=$(git tag --points-at ${SHA})
 
 if [ "$(git status --porcelain --untracked-files=no)" ]; then
-    SUFFIX="-$(uuidgen -r)"
+    SUFFIX="-$(uuidgen)"
 fi
 
 for TAG in $TAGS; do
   if [[ $TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo -n "${TAG}${SUFFIX}"
+    printf "${TAG}${SUFFIX}"
     exit 0
   fi
 done
 
-echo -n "${SHA}${SUFFIX}"
+printf "${SHA}${SUFFIX}"
