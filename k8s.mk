c515eb60132a68cfe5a907414061854793724e91
David Morrison <drmorr@appliedcomputing.io>
2024-10-25 09:13:22 -0700

    copy raw as part of k8s target

--- k8s.mk
+++ k8s.mk
@@ -10,6 +10,7 @@ pre-k8s:: | $(K8S_MANIFESTS_DIR)
 	if [[ -f "k8s/pyproject.toml" ]]; then cd k8s && poetry install; fi
 
 k8s: pre-k8s
+	cp -r k8s/raw $(K8S_MANIFESTS_DIR) || true
 	if [[ -f "k8s/pyproject.toml" ]]; then \
 	  	cd k8s && JSII_SILENCE_WARNING_UNTESTED_NODE_VERSION=1 CDK8S_OUTDIR=$(K8S_MANIFESTS_DIR) BUILD_DIR=$(BUILD_DIR) poetry run ./main.py; \
 	fi
@@ -19,6 +20,5 @@ kustomize: pre-k8s
 	cd k8s && JSII_SILENCE_WARNING_UNTESTED_NODE_VERSION=1 CDK8S_OUTDIR=$(KUSTOMIZE_DIR) BUILD_DIR=$(KUSTOMIZE_DIR) APP_VERSION=$(APP_VERSION) poetry run ./main.py --kustomize
 
 run: k8s
-	cp -r k8s/raw $(K8S_MANIFESTS_DIR) || true
 	kubectl apply -f $(K8S_MANIFESTS_DIR)/raw || true
 	kubectl apply -f $(K8S_MANIFESTS_DIR)
