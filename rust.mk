fc3a77d4d3ca9c102d000ebc34982ee7fcacf89e
David Morrison <drmorr@appliedcomputing.io>
2025-04-29 09:21:46 -0700

    updated rust setup/release

--- rust.mk
+++ rust.mk
@@ -1,5 +1,6 @@
 CARGO ?= cargo
 COVERAGE_IGNORES=
+EXCLUDE_CRATES=
 
 ifdef IN_CI
 RUST_COVER_TYPE = --codecov --output-path codecov.json
@@ -15,6 +16,11 @@ TEST_CMD=+nightly llvm-cov nextest --config-file $(CONFIG_DIR)/nextest.toml $(NE
 .PHONY: _version
 _version:
 	cargo version
+
+_setup::
+	cargo install --locked git-cliff
+	cargo install --locked cargo-workspaces
+	cargo install --locked cargo-edit@0.13.1
 
 _build:: _version
 
@@ -44,9 +50,9 @@ _cover::
 		$(if $(COVERAGE_IGNORES),--ignore-filename-regex "$(subst $(space),|,$(COVERAGE_IGNORES))",)
 
 .PHONY: release
-release: NEW_APP_VERSION=$(subst v,,$(shell git cliff --bumped-version))
+release: NEW_APP_VERSION=$(subst v,,$(shell git cliff -c $(CONFIG_DIR)/cliff.toml --bumped-version))
 release:
-	cargo set-version $(NEW_APP_VERSION)
+	cargo set-version $(NEW_APP_VERSION) $(if $(EXCLUDE_CRATES),--exclude $(EXCLUDE_CRATES),)
 	git cliff -c $(CONFIG_DIR)/cliff.toml -u --tag $(NEW_APP_VERSION) --prepend CHANGELOG.md
 	git commit -a -m "release: version v$(NEW_APP_VERSION)" && \
 		git tag v$(NEW_APP_VERSION)
